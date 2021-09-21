324d81865208e3643273cf2c13dea166e57dc3a1
richard-grin <grin@unice.fr>
2021-09-21 21:52:52 +0200

    Delete buildAndRun.bat

--- buildAndRun.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call mvn clean package
-call docker build -t fr.grin/tpCustomerGrin .
-call docker rm -f tpCustomerGrin
-call docker run -d -p 9080:9080 -p 9443:9443 --name tpCustomerGrin fr.grin/tpCustomerGrin
